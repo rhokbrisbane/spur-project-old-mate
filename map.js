4ddfe97b540b62af91399b0afc9eca47fb5665dd
Justin Indra Rahardjo <Justin Indra Rahardjo>
2016-06-05 12:32:27 +1000

    Style maps to be greyscaled

--- map.js
+++ map.js
@@ -1,9 +1,11 @@
 function initializeMaps(markers) {
 	var myLatLng = {lat: -25.2744, lng: 133.7751};
 
+	var mapStyle = [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}];
   var map = new google.maps.Map(document.getElementById('map_canvas'), {
     zoom: 4,
-    center: myLatLng
+    center: myLatLng,
+		styles: mapStyle
   });
 
 	var bounds = new google.maps.LatLngBounds();
