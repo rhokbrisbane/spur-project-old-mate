5c5f48b6d26069930f81c046a50b15cf9055d5f4
Lyndon Moore <moore.lyndon.robert@gmail.com>
2016-06-05 10:52:55 +1000

    Onehour Page
    Added Styling template to Onehour page and content.

---
+++ onehour.php
@@ -0,0 +1,235 @@
+<?php
+  include 'inc/header.php';
+?>
+
+<main class="wrapper">
+  <body>
+
+  <!-- One Hour Headings -->
+  <section class="titleBlock">
+    <article class="titleContent backFive">
+      <h1>One Hour</h1>
+      <h2>
+      Just one hour a month is all it takes to foster connectivity with an Old Mate.
+      </h2>
+    </article>
+  </section>
+
+  <!-- Tag Line -->
+  <section class="subheading">
+    <div>
+      <h2>Need some help with what to do for that hour? We have you covered:</h2>
+    </div>
+  </section>
+
+  <!-- Montly Old Mate Ideas -->
+  <section class="contentMain">
+    <article class="contentRow">
+      <div class="contentCenter">
+        <img class="imgCircle" src="img\actions-chess.jpeg">
+      </div>
+      <div class="contentCenter">
+        <h1>Month 1: Chess</h1>
+        <h2>
+          This is Roy. Roy lost his wife a few years ago and has been fairly isolated since. His family mostly live interstate,
+          and those who live close are always very busy.  Roy used to play Chess with his wife and really misses the games they had together.
+          Roy has said he would love for someone to come and play chess with him sometimes; as it’s one of his favourite pastimes.
+        </h2>
+      </div>
+    </article>
+  </section>
+  <section class="contentMain contentAlt">
+    <article class="contentRow">
+    <div class="contentCenter">
+      <h1>Month 2: Gardening</h1>
+      <h2>
+        Bob is Jill’s grandfather and he loves gardening. As Bob has gotten older, Bob isn’t as mobile as he used to be. Jill goes around to Bob’s
+        house for an hour a month and helps him with some of the trickier yard work that he can no longer do safely. Bob loves the time he gets
+        to spend with Jill and he also loves seeing his garden flourish! #oldmate
+      </h2>
+    </div>
+    <div class="contentCenter">
+      <img class="imgCircle" src="img\alone-beach.jpeg">
+    </div>
+    </article>
+  </section>
+  <section class="contentMain">
+    <article class="contentRow">
+      <div class="contentCenter">
+        <img class="imgCircle" src="img\alone-beach.jpeg">
+      </div>
+      <div class="contentCenter">
+      <h1>Month 3: Volunteering at an aged-care facility</h1>
+      <h2>
+        This is Geoff. Geoff’s family are overseas and the social interaction he gets comes only from activities organised by the aged car
+        facility where he lives. Geoff would never ask for help or support; it’s not how his generation was raised to operate, but Geoff
+        gets very lonely with no-one to interact with.   Take a pack of cards, your ipod or a chessboard and visit Geoff or someone like him
+        in your local aged care facility. #oldmate
+      </h2>
+      <h2>
+        It’s not just the Old Mates in your life that need someone, there are a lot of men in Australia who are in need of friends
+        and support networks. Volunteering at an aged-care facility is a great way to connect with Old Mates who really need it:
+      </h2>
+      </div>
+    </article>
+  </section>
+  <section class="contentMain contentAlt">
+    <article class="contentRow">
+      <div class="contentCenter">
+      <h1>Month 4: Cards</h1>
+      <h2>
+        Jacob is a huge fan of Canasta, but without people to join him, he’s not able to play! Grab a deck of cards, and maybe even bring a
+        friend; brighten Jacob or your Grandpa’s day by playing a round or two! If you want to learn how to play Canasta, see here. #oldmate
+      </h2>
+      </div>
+      <div class="contentCenter">
+        <img class="imgCircle" src="img\actions-chess.jpeg">
+      </div>
+    </article>
+  </section>
+  <section class="contentMain">
+    <article class="contentRow">
+      <div class="contentCenter">
+        <img class="imgCircle" src="img\actions-chess.jpeg">
+      </div>
+      <div class="contentCenter">
+      <h1>Month 5: Memoirs</h1>
+      <h2>
+        Jacob is a huge fan of Canasta, but without people to join him, he’s not able to play! Grab a deck of cards, and maybe even bring a
+        friend; brighten Jacob or your Grandpa’s day by playing a round or two! If you want to learn how to play Canasta, see here. #oldmate
+      </h2>
+      </div>
+    </article>
+  </section>
+  <section class="contentMain contentAlt">
+    <article class="contentRow">
+      <div class="contentCenter">
+      <h1>Month 6: Music</h1>
+      <h2>
+        Music is an amazing way of connecting people to memories and happy past emotions. (see this amazing catalyst documentary for more detail)
+        Ask them about their favourite music and build a playlist to listen together or set up for them.
+      </h2>
+      </div>
+      <div class="contentCenter">
+        <img class="imgCircle" src="img\actions-chess.jpeg">
+      </div>
+    </article>
+  </section>
+  <section class="contentMain">
+    <article class="contentRow">
+      <div class="contentCenter">
+        <img class="imgCircle" src="img\actions-chess.jpeg">
+      </div>
+      <div class="contentCenter">
+        <h1>Month 7: Get your work team involved</h1>
+        <h2>
+          Music is an amazing way of connecting people to memories and happy past emotions. (see this amazing catalyst documentary for more detail)
+          Ask them about their favourite music and build a playlist to listen together or set up for them.
+        </h2>
+      </div>
+    </article>
+  </section>
+  <section class="contentMain contentAlt">
+    <article class="contentRow">
+      <div class="contentCenter">
+      <h1>Month 8: Share a meal / cook a meal</h1>
+      <h2>
+        Music is an amazing way of connecting people to memories and happy past emotions. (see this amazing catalyst documentary for more detail)
+        Ask them about their favourite music and build a playlist to listen together or set up for them.
+      </h2>
+      </div>
+      <div class="contentCenter">
+        <img class="imgCircle" src="img\actions-chess.jpeg">
+      </div>
+    </article>
+  </section>
+  <section class="contentMain">
+    <article class="contentRow">
+      <div class="contentCenter">
+        <img class="imgCircle" src="img\actions-chess.jpeg">
+      </div>
+      <div class="contentCenter">
+      <h1>Month 9: Check out your local RSL activities</h1>
+      <h2>
+        Music is an amazing way of connecting people to memories and happy past emotions. (see this amazing catalyst documentary for more detail)
+        Ask them about their favourite music and build a playlist to listen together or set up for them.
+      </h2>
+      </div>
+    </article>
+  </section>
+  <section class="contentMain contentAlt">
+    <article class="contentRow">
+      <div class="contentCenter">
+      <h1>Month 10: Cup of tea &amp; photos</h1>
+      <h2>
+        Music is an amazing way of connecting people to memories and happy past emotions. (see this amazing catalyst documentary for more detail)
+        Ask them about their favourite music and build a playlist to listen together or set up for them.
+      </h2>
+      </div>
+      <div class="contentCenter">
+        <img class="imgCircle" src="img\actions-chess.jpeg">
+      </div>
+    </article>
+  </section>
+  <section class="contentMain">
+    <article class="contentRow">
+      <div class="contentCenter">
+        <img class="imgCircle" src="img\actions-chess.jpeg">
+      </div>
+      <div class="contentCenter">
+      <h1>Month 11: Wild Card hobby choice!</h1>
+      <h2>
+        Music is an amazing way of connecting people to memories and happy past emotions. (see this amazing catalyst documentary for more detail)
+        Ask them about their favourite music and build a playlist to listen together or set up for them.
+      </h2>
+      </div>
+    </article>
+    <section class="contentMain contentAlt">
+    <article classs="contentRow">
+      <div class="contentCenter">
+      <h1>Month 12: Cook a meal with #oldmate</h1>
+      <h2>
+        Music is an amazing way of connecting people to memories and happy past emotions. (see this amazing catalyst documentary for more detail)
+        Ask them about their favourite music and build a playlist to listen together or set up for them.
+      </h2>
+      </div>
+      <div class="contentCenter">
+        <img class="imgCircle" src="img\actions-chess.jpeg">
+      </div>
+    </article>
+    </section>
+
+    <!-- Tips and Tricks -->
+    <section class="contentMain">
+      <article class="contentRow">
+      <div class="contentCenter">
+        <h1>Tips:</h1>
+        <h2>Your participation is not as a counsellor or psychologist: It’s simply as a friend. If your Old Mate is in need of mental health support,
+          here are a range of services to direct them to:</h2>
+        <ul>
+          <li>Lifeline	: 13 11 14</li>
+          <li>Beyondblue: 1300 22 4636</li>
+          <li>My Aged Care: 1800 200 422</li>
+          <li>SANE Australia: 1800 18 7263</li>
+        </ul>
+      </div>
+      <div class="contentCenter">
+        <h2>
+          Volunteer visitors may share stories, go for a stroll, listen to music or play a game. As a CVS volunteer you play a vital role in
+          improving the quality of life of the person through ongoing companionship.
+        </h2>
+        <h2>
+          <a href="https://www.volunteer.com.au/FindVolunteers">https://www.volunteer.com.au/FindVolunteers</a>
+          <br>
+          <a href="https://www.volunteer.com.au/Opportunity/Details/85343/telechat-volunteer-caller">https://www.volunteer.com.au/Opportunity/Details/85343/telechat-volunteer-caller</a>
+        </h2>
+      </div>
+    </article>
+  </section>
+</body>
+</main>
+</html>
+
+<?php
+include 'inc/footer.php';
+?>
