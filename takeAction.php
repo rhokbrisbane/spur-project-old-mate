9c0ee9c4758d5e4312e289ba16e9c56a5b28b2b1
Jahl Herapath <jahl.herapath@gmail.com>
2016-06-05 13:10:41 +1000

    Minor Formatting
    Minor formatting fixes

--- takeAction.php
+++ takeAction.php
@@ -31,7 +31,8 @@
       <div class="contentCenter">
         <h1>Month 1: Chess</h1>
         <h2>
-          Playing chess with your Old Mate is an easy way to spend some quality time together and chances are your Old Mate will teach you a thing or two about the game. It’s also a nice way to start having meaningful conversations. <a href="https://www.chess.com/learn-how-to-play-chess" target="_blank">Learn how to Play</a>. #oldmate
+          Playing chess with your Old Mate is an easy way to spend some quality time together and chances are your Old Mate will teach you a thing or two about the game. </h2><br/>
+          <h2>It’s also a nice way to start having meaningful conversations. <a href="https://www.chess.com/learn-how-to-play-chess" target="_blank">Learn how to Play</a>. #oldmate
         </h2>
       </div>
     </article>
@@ -41,7 +42,10 @@
     <div class="contentCenter altText">
       <h1>Month 2: Gardening</h1>
       <h2>
-        Chances are your Old Mate will have a green thumb, but in their older years they may not be as mobile as they used to be. Take an hour to stop over to your Old Mates house and help him with some of the trickier yard work that he can no longer do safely. It’s a great way to spend time with your Old Mate as well as helping them look after their home and garden! #oldmate
+        Chances are your Old Mate will have a green thumb, but in their older years they may not be as mobile as they used to be. </h2><br/>
+          <h2>Take an hour to stop over to your Old Mates house and help him with some of the trickier yard work that he can no longer do safely. 
+        </h2><br/>
+          <h2>It’s a great way to spend time with your Old Mate as well as helping them look after their home and garden! #oldmate
       </h2>
     </div>
     <div class="contentCenter">
@@ -55,9 +59,10 @@
         <img class="imgCircle" src="img\ActionsVolunteering.jpg">
       </div>
       <div class="contentCenter">
-      <h1>Month 3: Volunteering at an aged-care facility</h1>
+      <h1>Month 3: Volunteering </h1>
       <h2>
-        It’s not just the Old Mates in your life that need someone, there are a lot of men in Australia who are in need of friends and support networks. Volunteering at an aged-care facility is a great way to connect with Old Mates who really need. <a href="http://www.redcross.org.au/community-visitors-scheme.aspx" target="_blank">Find locations near you</a>. #oldmate
+        It’s not just the Old Mates in your life that need someone, there are a lot of men in Australia who are in need of friends and support networks. </h2><br/>
+          <h2>Volunteering at an aged-care facility is a great way to connect with Old Mates who really need. <a href="http://www.redcross.org.au/community-visitors-scheme.aspx" target="_blank">Find locations near you</a>. #oldmate
       </h2>
       </div>
     </article>
@@ -67,7 +72,8 @@
       <div class="contentCenter altText">
       <h1>Month 4: Old Mate Double-Date!</h1>
       <h2>
-        Set up a meal or a coffee with your Old Mate, a friend and their Old Mate and let the good times roll! It’s a great activity to tackle with a friend as it may require a little travel and organisation. #oldmate
+        Set up a meal or a coffee with your Old Mate, a friend and their Old Mate and let the good times roll! </h2><br/>
+          <h2>It’s a great activity to tackle with a friend as it may require a little travel and organisation. #oldmate
       </h2>
       </div>
       <div class="contentCenter">
@@ -93,7 +99,8 @@
       <div class="contentCenter altText">
       <h1>Month 6: Memoir</h1>
       <h2>
-        Take the time to ask your Old Mate about their life, their history and some of the exciting experiences they have had. Of course keep in mind that some people may not want to share too much; these stories may remind them of people they’ve lost or activities they can’t do anymore, so be mindful of this in each instance. #oldmate
+        Take the time to ask your Old Mate about their life, their history and some of the exciting experiences they have had.</h2><br/>
+          <h2> Of course, keep in mind that some people may not want to share too much; these stories may remind them of people they’ve lost or activities they can’t do anymore, so be mindful of this in each instance. #oldmate
       </h2>
       </div>
       <div class="contentCenter">
@@ -120,7 +127,8 @@
       <h1>Month 8: Get your friends and family involved!
       </h1>
       <h2>
-        We know you’ve been doing an amazing job with your Old Mate so now it’s time for you to get some of your friends, family or the people you work with involved. Challenge them to spend an hour with the an Old Mate in their life. At the end of the month, get together and share your experiences and any photos you’ve taken, and share them to the #oldmate website!
+        We know you’ve been doing an amazing job with your Old Mate so now it’s time for you to get some of your friends, family or the people you work with involved. </h2><br/>
+          <h2>Challenge them to spend an hour with the an Old Mate in their life. At the end of the month, get together and share your experiences and any photos you’ve taken, and share them to the #oldmate website!
       </h2>
       </div>
       <div class="contentCenter">
@@ -146,7 +154,8 @@
       <div class="contentCenter altText">
       <h1>Month 10: Check out your local RSL and community activities!</h1>
       <h2>
-        Local RSL and community clubs run all sorts of fun social events like bingo, music nights, market stalls and general social gatherings. Call your local club to see what they have going on this month and line up your Old Mate to come along with you and reap the benefits! #oldmate
+        Local RSL and community clubs run all sorts of fun social events like bingo, music nights, market stalls and general social gatherings. </h2><br/>
+          <h2>Call your local club to see what they have going on this month and line up your Old Mate to come along with you and reap the benefits! #oldmate
       </h2>
       </div>
       <div class="contentCenter">
@@ -162,7 +171,8 @@
       <div class="contentCenter">
       <h1>Month 11: Cup of tea &amp; photos!</h1>
       <h2>
-        Your Old Mate grew up pre-instagram, so he’s likely to have a lot of physical photos around that he’d love to share! Ask him to get some of them out and you’ll find out all sorts of interesting stories about him, his friends and his life #oldmate
+        Your Old Mate grew up pre-instagram, so he’s likely to have a lot of physical photos around that he’d love to share! </h2><br/>
+          <h2>Ask him to get some of them out and you’ll find out all sorts of interesting stories about him, his friends and his life #oldmate
       </h2>
       </div>
     </article>
@@ -171,7 +181,8 @@
       <div class="contentCenter altText">
       <h1>Month 12: Wild card hobby choice!</h1>
       <h2>
-        Ask your Old Mate what they want to do. What do they love? What have they done historically but haven’t done for ages? Take this opportunity to take your Old Mate out for lawn bowls, bird watching, tea or even a round of poker! You never know what adventure you might end up having. #oldmate
+        Ask your Old Mate what they want to do. What do they love? What have they done historically but haven’t done for ages? </h2><br/>
+          <h2>Take this opportunity to take your Old Mate out for lawn bowls, bird watching, tea or even a round of poker! You never know what adventure you might end up having. #oldmate
       </h2>
       </div>
       <div class="contentCenter">
