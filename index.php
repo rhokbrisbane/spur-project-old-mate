5d221b2a7d2d7203272215e01d970de03f48105b
Lyndon Moore <moore.lyndon.robert@gmail.com>
2016-06-05 12:25:07 +1000

    One hour Page
    One hour content finalised

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
   include 'inc/header.php';
 ?>
 
@@ -21,7 +21,7 @@
         </div>
         <div class="contentCenter">
           <h1>1 Hour Per Month</h1>
-          <h2>Elderly men take their own lives at two times the national average. With just one hour per month, we can make a change.</h2>                
+          <h2>Elderly men take their own lives at two times the national average. With just one hour per month, we can make a change.</h2>
         </div>
       </div>
     </section>
@@ -38,9 +38,9 @@
       <div class="contentRow">
         <div class="contentCenter">
           <h1>Helping Old Mates</h1>
-          <h2>I hereby take the pledge to spend at least one hour per month with an old mate for the next twelve months.</h2>   
+          <h2>I hereby take the pledge to spend at least one hour per month with an old mate for the next twelve months.</h2>
         </div>
-        <div class="contentCenter">      
+        <div class="contentCenter">
           <div id="mc_embed_signup">
           <form action="//spurprojects.us2.list-manage.com/subscribe/post?u=c319b91ac25607f4451c84e2b&amp;id=a34e1c5eb6" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
           <div id="mc_embed_signup_scroll">
@@ -93,38 +93,15 @@
         <div class="contentGrid">
           <div class="contentStyle">
             <p>Just spent the day fishing with #oldmate gotta love a bit of sunshine!</p>
-            <?php
-              require "<div class=""></div>/vendor/autoload.php";
-              use Abraham\TwitterOAuth\TwitterOAuth;
-
-              define('CONSUMER_KEY', 'bZBePzKUIAgUD67Q5S1XyAt8d');
-              define('CONSUMER_SECRET', 'eIEhPq4BGay9P335fcPpMm8cPezGxnV7qr65E6z9PigqzYb6hH');
-              define('ACCESS_TOKEN', '2903074441-gk3jRwnbCsyGCQ1N0w0wFXSZGT6h5efduPNFZoK');
-              define('ACCESS_TOKEN_SECRET', 'MQpn5ObworbuYy4SnpHrw1FhD894Z8KwHovCdOqtyCR4g');
-
-              $toa = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
-
-              $query = array(
-                "q" => "#oldmate",
-                "result_type" => "recent"
-              );
-
-              $results = $toa->get('search/tweets', $query);
-
-              foreach ($results->statuses as $result) {
-                echo "<p>" . $result->user->screen_name . ": " . $result->text . "\n". "</p>";
-              }
-
-            ?>
           </div>
         </div>
-    
+
     </section>
 
   </body>
 </main>
 </html>
 
-<?php 
+<?php
   include 'inc/footer.php';
 ?>
--- onehour.php
+++ onehour.php
@@ -10,6 +10,7 @@
     <article class="titleContent backFive">
       <h5>One Hour</h5>
       <h2>Just one hour a month is all it takes to foster connectivity with an Old Mate.</h2>
+      <p>That's just 0.13% of your month</p>
       </h2>
     </article>
   </section>
@@ -30,9 +31,7 @@
       <div class="contentCenter">
         <h1>Month 1: Chess</h1>
         <h2>
-          This is Roy. Roy lost his wife a few years ago and has been fairly isolated since. His family mostly live interstate,
-          and those who live close are always very busy.  Roy used to play Chess with his wife and really misses the games they had together.
-          Roy has said he would love for someone to come and play chess with him sometimes; as it’s one of his favourite pastimes.
+          Playing chess with your Old Mate is an easy way to spend some quality time together and chances are your Old Mate will teach you a thing or two about the game. It’s also a nice way to start having meaningful conversations. <a href="https://www.chess.com/learn-how-to-play-chess" target="_blank">Learn how to Play</a>. #oldmate
         </h2>
       </div>
     </article>
@@ -42,9 +41,7 @@
     <div class="contentCenter">
       <h1>Month 2: Gardening</h1>
       <h2>
-        Bob is Jill’s grandfather and he loves gardening. As Bob has gotten older, Bob isn’t as mobile as he used to be. Jill goes around to Bob’s
-        house for an hour a month and helps him with some of the trickier yard work that he can no longer do safely. Bob loves the time he gets
-        to spend with Jill and he also loves seeing his garden flourish! #oldmate
+        Chances are your Old Mate will have a green thumb, but in their older years they may not be as mobile as they used to be. Take an hour to stop over to your Old Mates house and help him with some of the trickier yard work that he can no longer do safely. It’s a great way to spend time with your Old Mate as well as helping them look after their home and garden! #oldmate
       </h2>
     </div>
     <div class="contentCenter">
@@ -60,14 +57,7 @@
       <div class="contentCenter">
       <h1>Month 3: Volunteering at an aged-care facility</h1>
       <h2>
-        This is Geoff. Geoff’s family are overseas and the social interaction he gets comes only from activities organised by the aged car
-        facility where he lives. Geoff would never ask for help or support; it’s not how his generation was raised to operate, but Geoff
-        gets very lonely with no-one to interact with.   Take a pack of cards, your ipod or a chessboard and visit Geoff or someone like him
-        in your local aged care facility. #oldmate
-      </h2>
-      <h2>
-        It’s not just the Old Mates in your life that need someone, there are a lot of men in Australia who are in need of friends
-        and support networks. Volunteering at an aged-care facility is a great way to connect with Old Mates who really need it:
+        It’s not just the Old Mates in your life that need someone, there are a lot of men in Australia who are in need of friends and support networks. Volunteering at an aged-care facility is a great way to connect with Old Mates who really need. <a href="http://www.redcross.org.au/community-visitors-scheme.aspx" target="_blank">Find locations near you</a>. #oldmate
       </h2>
       </div>
     </article>
@@ -75,10 +65,9 @@
   <section class="contentMain contentAlt">
     <article class="contentRow">
       <div class="contentCenter">
-      <h1>Month 4: Cards</h1>
+      <h1>Month 4: Old Mate Double-Date!</h1>
       <h2>
-        Jacob is a huge fan of Canasta, but without people to join him, he’s not able to play! Grab a deck of cards, and maybe even bring a
-        friend; brighten Jacob or your Grandpa’s day by playing a round or two! If you want to learn how to play Canasta, see here. #oldmate
+        Set up a meal or a coffee with your Old Mate, a friend and their Old Mate and let the good times roll! It’s a great activity to tackle with a friend as it may require a little travel and organisation. #oldmate
       </h2>
       </div>
       <div class="contentCenter">
@@ -92,10 +81,9 @@
         <img class="imgCircle" src="img\actions-chess.jpeg">
       </div>
       <div class="contentCenter">
-      <h1>Month 5: Memoirs</h1>
+      <h1>Month 5: Cards</h1>
       <h2>
-        Jacob is a huge fan of Canasta, but without people to join him, he’s not able to play! Grab a deck of cards, and maybe even bring a
-        friend; brighten Jacob or your Grandpa’s day by playing a round or two! If you want to learn how to play Canasta, see here. #oldmate
+        Grab a deck of cards, and maybe bring a friend; brighten your Old Mate's day by playing a round or two! <a href="http://www.canasta.net/canasta_rules.php" target="_blank">Learn how to play Canasta</a> #oldmate
       </h2>
       </div>
     </article>
@@ -103,10 +91,9 @@
   <section class="contentMain contentAlt">
     <article class="contentRow">
       <div class="contentCenter">
-      <h1>Month 6: Music</h1>
+      <h1>Month 6: Memoir</h1>
       <h2>
-        Music is an amazing way of connecting people to memories and happy past emotions. (see this amazing catalyst documentary for more detail)
-        Ask them about their favourite music and build a playlist to listen together or set up for them.
+        Take the time to ask your Old Mate about their life, their history and some of the exciting experiences they have had. Of course keep in mind that some people may not want to share too much; these stories may remind them of people they’ve lost or activities they can’t do anymore, so be mindful of this in each instance. #oldmate
       </h2>
       </div>
       <div class="contentCenter">
@@ -120,21 +107,20 @@
         <img class="imgCircle" src="img\actions-chess.jpeg">
       </div>
       <div class="contentCenter">
-        <h1>Month 7: Get your work team involved</h1>
+        <h1>Month 7: Music</h1>
         <h2>
-          Music is an amazing way of connecting people to memories and happy past emotions. (see this amazing catalyst documentary for more detail)
-          Ask them about their favourite music and build a playlist to listen together or set up for them.
-        </h2>
+          Music is an <a href="http://www.abc.net.au/catalyst/stories/4421003.htm" target="_blank">amazing way</a> of connecting people to life and memories. Ask them about their favourite music and build a playlist to listen together or set one up for them. Spotify, Youtube or iTunes are great and easy ways to do this.
+  </h2>
       </div>
     </article>
   </section>
   <section class="contentMain contentAlt">
     <article class="contentRow">
       <div class="contentCenter">
-      <h1>Month 8: Share a meal / cook a meal</h1>
+      <h1>Month 8: Get your friends and family involved!
+      </h1>
       <h2>
-        Music is an amazing way of connecting people to memories and happy past emotions. (see this amazing catalyst documentary for more detail)
-        Ask them about their favourite music and build a playlist to listen together or set up for them.
+        We know you’ve been doing an amazing job with your Old Mate so now it’s time for you to get some of your friends, family or the people you work with involved. Challenge them to spend an hour with the an Old Mate in their life. At the end of the month, get together and share your experiences and any photos you’ve taken, and share them to the #oldmate website!
       </h2>
       </div>
       <div class="contentCenter">
@@ -148,10 +134,9 @@
         <img class="imgCircle" src="img\actions-chess.jpeg">
       </div>
       <div class="contentCenter">
-      <h1>Month 9: Check out your local RSL activities</h1>
+      <h1>Month 9: Share a Meal</h1>
       <h2>
-        Music is an amazing way of connecting people to memories and happy past emotions. (see this amazing catalyst documentary for more detail)
-        Ask them about their favourite music and build a playlist to listen together or set up for them.
+        Find out what your Old Mate <a href="http://www.epicurious.com" target="_blank">loves to eat</a> and either cook it up with them, or order take out. Use this as an opportunity to chat about life, common interests, world news and more. #oldmate
       </h2>
       </div>
     </article>
@@ -159,10 +144,9 @@
   <section class="contentMain contentAlt">
     <article class="contentRow">
       <div class="contentCenter">
-      <h1>Month 10: Cup of tea &amp; photos</h1>
+      <h1>Month 10: Check out your local RSL and community activities!</h1>
       <h2>
-        Music is an amazing way of connecting people to memories and happy past emotions. (see this amazing catalyst documentary for more detail)
-        Ask them about their favourite music and build a playlist to listen together or set up for them.
+        Local RSL and community clubs run all sorts of fun social events like bingo, music nights, market stalls and general social gatherings. Call your local club to see what they have going on this month and line up your Old Mate to come along with you and reap the benefits! #oldmate
       </h2>
       </div>
       <div class="contentCenter">
@@ -176,20 +160,19 @@
         <img class="imgCircle" src="img\actions-chess.jpeg">
       </div>
       <div class="contentCenter">
-      <h1>Month 11: Wild Card hobby choice!</h1>
+      <h1>Month 11: Cup of tea &amp; photos!</h1>
       <h2>
-        Music is an amazing way of connecting people to memories and happy past emotions. (see this amazing catalyst documentary for more detail)
-        Ask them about their favourite music and build a playlist to listen together or set up for them.
+        Your Old Mate grew up pre-instagram, so he’s likely to have a lot of physical photos around that he’d love to share! Ask him to get some of them out and you’ll find out all sorts of interesting stories about him, his friends and his life #oldmate
       </h2>
       </div>
     </article>
     <section class="contentMain contentAlt">
     <article classs="contentRow">
       <div class="contentCenter">
-      <h1>Month 12: Cook a meal with #oldmate</h1>
+      <h1>Month 12: Wild card hobby choice!
+      </h1>
       <h2>
-        Music is an amazing way of connecting people to memories and happy past emotions. (see this amazing catalyst documentary for more detail)
-        Ask them about their favourite music and build a playlist to listen together or set up for them.
+        Ask your Old Mate what they want to do. What do they love? What have they done historically but haven’t done for ages? Take this opportunity to take your Old Mate out for lawn bowls, bird watching, tea or even a round of poker! You never know what adventure you might end up having. #oldmate
       </h2>
       </div>
       <div class="contentCenter">
@@ -202,26 +185,14 @@
     <section class="contentMain contentColor">
       <article class="contentRow">
       <div class="contentCenter">
-        <h1>Tips:</h1>
-        <h2>Your participation is not as a counsellor or psychologist: It’s simply as a friend. If your Old Mate is in need of mental health support,
-          here are a range of services to direct them to:</h2>
+        <h1>Remember!</h1>
+        <h2>Your participation is not as a counsellor or psychologist: It’s simply as a friend. If your Old Mate is in need of mental health support, here are a range of services to direct them to:</h2>
         <ul>
           <li>Lifeline	: 13 11 14</li>
           <li>Beyondblue: 1300 22 4636</li>
           <li>My Aged Care: 1800 200 422</li>
           <li>SANE Australia: 1800 18 7263</li>
         </ul>
-      </div>
-      <div class="contentCenter">
-        <h2>
-          Volunteer visitors may share stories, go for a stroll, listen to music or play a game. As a CVS volunteer you play a vital role in
-          improving the quality of life of the person through ongoing companionship.
-        </h2>
-        <h2>
-          <a href="https://www.volunteer.com.au/FindVolunteers">https://www.volunteer.com.au/FindVolunteers</a>
-          <br>
-          <a href="https://www.volunteer.com.au/Opportunity/Details/85343/telechat-volunteer-caller">https://www.volunteer.com.au/Opportunity/Details/85343/telechat-volunteer-caller</a>
-        </h2>
       </div>
     </article>
   </section>
